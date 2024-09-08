3f7bf7f993251f75cb82f4a01a0bb5679b8e93d7
Y0GESHSHIND <yogeshshinde3624@gmail.com>
2024-09-08 13:17:36 +0530

    Second module Finish

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 function CharCount(){
    var textarea = document.getElementById("userInput");
    var text = textarea.value;
-   console.log(text);
+   // console.log(typeof text);
    document.getElementById("Char").innerHTML=text.length;
-   
+   numbersCount(text);
+}
+
+function numbersCount(text){
+   let count =0;
+   for(let value of text){
+      if(!isNaN(Number(value)) && value.trim() !== ""){
+         count++;
+      }
+   }
+
+   document.getElementById("Number").innerHTML= count;
 }
